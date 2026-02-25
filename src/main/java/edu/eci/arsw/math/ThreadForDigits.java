c770e2ab314b7049fb8cf1f891adca30c78702c2
unknown <ivan.cubillos-v@labinfo.is.escuelaing.edu.co>
2026-02-25 18:02:21 -0500

    Clase Thread

---
+++ src/main/java/edu/eci/arsw/math/ThreadForDigits.java
@@ -0,0 +1,17 @@
+package edu.eci.arsw.math;
+
+public class ThreadForDigits extends Thread {
+
+    private int start;
+    private int finish;
+
+    ThreadForDigits(int start, int finish) {
+        this.start = start;
+        this.finish = finish;
+    }
+
+    @Override
+    public void run() {
+        
+    }
+}
